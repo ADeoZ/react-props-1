81fbfa3ff6b4898fac95ada6b59c288434c849b5
Aleksey Denisov <deoz@yandex.ru>
2021-07-20 19:54:12 +0300

    Init

---
+++ src/components/Stars.js
@@ -0,0 +1,28 @@
+import Star from "./Star";
+import PropTypes from 'prop-types';
+import { nanoid } from 'nanoid'
+
+export default function Stars({ count }) {
+  if (typeof count !== 'number' || isNaN(count) || count < 1 || count > 5) {
+    return null;
+  };
+
+  const starsElements = [];
+  for (let i = 0; i < count ; i += 1) {
+    starsElements.push(<Star key={nanoid(10)}/>);
+  }
+  
+  console.log(count);
+
+  return (
+    <ul className="card-body-stars u-clearfix">
+      <li>
+        {starsElements}
+      </li>
+    </ul>
+  );
+}
+
+Stars.propTypes = {
+  count: PropTypes.number.isRequired,
+}
